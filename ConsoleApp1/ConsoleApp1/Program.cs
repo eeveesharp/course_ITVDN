b7ad029a0755ffb20f3206b15cc2ed55dd7cee47
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-01-24 17:49:23 +0300

    try readonly in construction

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public readonly string field = "hello";
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.WriteLine(program.field);
+        }
+    }
+}
