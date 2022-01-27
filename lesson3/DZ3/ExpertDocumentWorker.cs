c2742ea2985904329e148f06173cb0cd72075e0c
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-01-27 19:01:48 +0300

    added task3

---
+++ lesson3/DZ3/ExpertDocumentWorker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ3
+{
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
---
+++ lesson3/DZ3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ключ");
+
+            string key = (Console.ReadLine()).ToString();
+
+            DocumentWorker document = null;
+
+            switch (key)
+            {
+                case "pro":
+                    {
+                        document = new ProDocumentWorker();
+                        break;
+                    }
+                case "exp":
+                    {
+                        document = new ExpertDocumentWorker();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ключ не верный");
+                        document = new DocumentWorker();
+                        break;
+                    }
+            }
+
+            document.OpenDocument();
+            document.EditDocument();
+            document.SaveDocument();
+        }
+    }
+}
