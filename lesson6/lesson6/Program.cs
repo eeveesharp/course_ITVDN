3c6310e5e205e11f2a921dc8975f3e62b65f6854
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-02-01 19:46:28 +0300

    vss

---
+++ lesson6/lesson6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            StaticClass.Plus(4, 5);
+        }
+    }
+}
---
+++ lesson6/lesson6/StaticClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson6
+{
+    static class StaticClass
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Вызов методы в статик классе");
+        }
+
+        public static double Plus(double a,double b)
+        {
+            double sum = a + b;
+
+            return sum;
+        }
+    }
+}
