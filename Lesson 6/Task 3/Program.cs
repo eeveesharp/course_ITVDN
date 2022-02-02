1294a125b7947e6fb5046e4497e4d4bcaa957647
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-02-02 16:09:09 +0300

    ftg

---
+++ Lesson 6/Task 3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Book
+    {
+        public class Note
+        {
+            string text = "";
+            public string Text
+            {
+                get { return text; }
+                set
+                {
+                    if (text != "")
+                        text += "\n";
+                    text += value;
+                }
+
+            }
+        }
+
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Поиск строки : " + str);
+        }
+
+    }
+
+    static class FindAndReplaceManager
+    {
+        static public void FindNext(string str)
+        {
+            Book a = new Book();
+            a.FindNext(str);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Book.Note note = new Book.Note();
+
+            note.Text = "";
+            note.Text = "I like it!";
+
+            FindAndReplaceManager.FindNext("nsd");
+
+            Console.WriteLine(note.Text);
+
+            // Delay.
+            Console.ReadKey();
+
+        }
+    }
+}
